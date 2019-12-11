432a3e7e40ad4c416d3d7ed53de043b6bd1e5caa
James Addison <james@reciperadar.com>
2019-12-11 16:58:11 +0000

    Fixup: revert accidental tfidf score change commit

--- scripts/product_graph.py
+++ scripts/product_graph.py
@@ -68,7 +68,7 @@ class ProductGraph(object):
             if term[0] in clearwords:
                 continue
             tfidf = self.index.get_total_tfidf(term)
-            if tfidf < 50:
+            if tfidf < 100:
                 continue
             # TODO: Likely inefficient; stopwords have high doc counts
             if self.exact_match_exists(term):
