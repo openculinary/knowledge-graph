9a8eef4f8eb08e61bf3ed5b5bd743faddbe23430
James Addison <james@reciperadar.com>
2019-12-11 00:37:24 +0000

    Add spelling correction & initial data

--- scripts/product.py
+++ scripts/product.py
@@ -4,11 +4,19 @@ import snowballstemmer
 
 stemmer = snowballstemmer.stemmer('english')
 
+misspellings = {}
+with open('scripts/data/misspellings.txt') as f:
+    for line in f.readlines():
+        if line.startswith('#'):
+            continue
+        spelling, correction = line.strip().split(',')
+        misspellings[spelling] = correction
+
 
 class Product(object):
 
     def __init__(self, name, frequency):
-        self.name = name
+        self.name = self.spell_correct(name)
         self.frequency = frequency
 
         self.depth = None
@@ -20,6 +28,12 @@ class Product(object):
     def __add__(self, other):
         self.frequency += other.frequency
         return self
+
+    @staticmethod
+    def spell_correct(name):
+        words = name.split(' ')
+        words = [misspellings.get(word) or word for word in words]
+        return ' '.join(words)
 
     @property
     def id(self):
