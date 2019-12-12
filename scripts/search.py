bea5be9e576bef028736d9773f85c7213f8c229f
James Addison <james@reciperadar.com>
2019-12-12 10:09:23 +0000

    Use native `hashedindex` stemmer support (#2)

--- scripts/search.py
+++ scripts/search.py
@@ -1,20 +1,26 @@
 from collections import defaultdict
 
-from hashedindex import HashedIndex, textparser
+from hashedindex import HashedIndex
+from hashedindex.textparser import word_tokenize
 from snowballstemmer import stemmer
 
-stemmer_en = stemmer('english')
+
+class SnowballStemmer():
+
+    stemmer_en = stemmer('english')
+
+    def stem(self, x):
+        return self.stemmer_en.stemWord(x)
 
 
 def tokenize(doc, stopwords=None):
     stopwords = stopwords or []
-    words = doc.split(' ')
-    # TODO: Push stemming into upstream hashedindex library
-    words = stemmer_en.stemWords(words)
-    doc = ' '.join(words)
-    for ngrams in range(len(words), 0, -1):
-        for term in textparser.word_tokenize(doc, stopwords, ngrams):
-            yield tuple(stemmer_en.stemWords(term))
+    stemmer = SnowballStemmer()
+
+    word_count = len(doc.split(' '))
+    for ngrams in range(word_count, 0, -1):
+        for term in word_tokenize(doc, stopwords, ngrams, stemmer=stemmer):
+            yield term
 
 
 def add_to_search_index(index, doc_id, doc, stopwords):
