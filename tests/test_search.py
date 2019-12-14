b187a864c8e1583ccade4ce749fee2ccf2872e2a
James Addison <james@reciperadar.com>
2019-12-14 18:55:28 +0000

    Add 'mayonnaise issue' test case

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,7 +1,9 @@
+from web.models.product import Product
 from web.search import (
     build_search_index,
     add_to_search_index,
     exact_match_exists,
+    execute_queries,
     tokenize,
 )
 
@@ -22,6 +24,18 @@ def test_token_stemming():
     tokens = list(tokenize(content, stopwords))
 
     assert tokens[0] == ('onion',)
+
+
+def test_stemming_consistency():
+    # some words change more than once under repeated snowball stemming
+    # i.e. mayonnaise -> mayonnais -> mayonnai
+    content = Product(name='mayonnaise', frequency=1)
+
+    index = build_search_index()
+    add_to_search_index(index, 0, content.content)
+    hits = execute_queries(index, [content.name])
+
+    assert hits
 
 
 def test_exact_match():
