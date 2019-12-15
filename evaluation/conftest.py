30e98692ab452c3303c3e014dec06f2495ff9faa
James Addison <james@reciperadar.com>
2019-12-15 17:43:21 +0000

    Improve evaluation test timing

--- evaluation/conftest.py
+++ evaluation/conftest.py
@@ -2,7 +2,11 @@ import pytest
 
 from web.app import app
 
+# Make a request to the flask application to force it to initialize
+test_client = app.test_client()
+test_client.get('/')
+
 
 @pytest.fixture
 def client():
-    return app.test_client()
+    return test_client
