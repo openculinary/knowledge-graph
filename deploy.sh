b4a100c0cb17046e3fec0498ebf562e3e7615c6d
James Addison <james@reciperadar.com>
2019-12-23 14:28:15 +0000

    Update deploy script image reference

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ SERVICE=$(basename `git rev-parse --show-toplevel`)
 COMMIT=$(git rev-parse --short HEAD)
 
 kubectl apply -f k8s
-kubectl set image deployments -l app=${SERVICE} ${SERVICE}=registry.gitlab.com/openculinary/${SERVICE}:${COMMIT}
+kubectl set image deployments -l app=${SERVICE} ${SERVICE}=registry.openculinary.org/reciperadar/${SERVICE}:${COMMIT}
