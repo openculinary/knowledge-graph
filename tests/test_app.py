044dc65db263465417b87d70a8123dff19a1a641
James Addison <james@reciperadar.com>
2020-04-21 19:18:36 +0100

    Add simple ingredient query response test

---
+++ tests/test_app.py
@@ -0,0 +1,23 @@
+from mock import patch
+
+from web.models.product import Product
+
+
+@patch('web.app.retrieve_hierarchy')
+@patch('web.app.retrieve_stopwords')
+def test_ingredient_query(stopwords, hierarchy, client):
+    stopwords.return_value = []
+    hierarchy.return_value = [
+        Product(name='onion', frequency=10, parent_id=None),
+    ]
+    expected_products = {
+        'large onion, diced, ': 'onion',
+    }
+
+    results = client.post(
+        '/ingredients/query',
+        data={'descriptions[]': list(expected_products.keys())}
+    ).json['results']
+
+    for description, product in expected_products.items():
+        assert results[description]['product'] == product
