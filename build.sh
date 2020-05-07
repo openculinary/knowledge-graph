b67349d5d199612867ae9721933c08f546d56abd
James Addison <james@reciperadar.com>
2020-05-07 19:53:35 +0100

    Add 'git' dependency at container build-time

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ container=$(buildah from docker.io/library/python:3.7-alpine)
 buildah copy ${container} 'web' 'web'
 buildah copy ${container} 'Pipfile'
 buildah run ${container} -- pip install pipenv --
+buildah run ${container} -- apk add --no-cache git --
 buildah run ${container} -- pipenv install --
+buildah run ${container} -- apk del git --
 buildah config --port 80 --entrypoint 'pipenv run gunicorn web.app:app --bind :80' ${container}
 buildah commit --squash --rm ${container} ${IMAGE_NAME}:${IMAGE_COMMIT}
