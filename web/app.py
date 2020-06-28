e70a69c1ae57f198083451f91240514776b1921a
James Addison <james@reciperadar.com>
2020-06-28 17:54:58 +0100

    Refactor: extract ingredient endpoint into separate module

--- web/app.py
+++ web/app.py
@@ -1,20 +1,10 @@
-from collections import defaultdict
-
-from flask import Flask, jsonify, request
-from hashedixsearch import (
-    add_to_search_index,
-    build_search_index,
-    execute_queries,
-    execute_query,
-    highlight,
-)
+from flask import Flask
 
 from web.loader import (
     CACHE_PATHS,
     retrieve_hierarchy,
     retrieve_stopwords,
 )
-from web.models.product import Product
 from web.models.product_graph import ProductGraph
 
 app = Flask(__name__)
@@ -33,76 +23,4 @@ def preload_graph():
     app.stopwords = app.graph.filter_stopwords()
 
 
-def find_product_candidates(products):
-    queries = [product.name for product in products]
-    results = execute_queries(
-        index=app.graph.index,
-        queries=queries,
-        stopwords=app.stopwords,
-        stemmer=Product.stemmer,
-        synonyms=Product.canonicalizations,
-        query_limit=-1
-    )
-    for description, hits in results:
-        for hit in hits:
-            product = app.graph.products_by_id[hit['doc_id']]
-            yield product
-
-
-@app.route('/ingredients/query', methods=['POST'])
-def query():
-    descriptions = request.form.getlist('descriptions[]')
-    products = [Product(name=description) for description in descriptions]
-
-    # Build a local search index over the product descriptions
-    description_index = build_search_index()
-    for doc_id, product in enumerate(products):
-        add_to_search_index(
-            index=description_index,
-            doc_id=doc_id,
-            doc=product.name,
-            stemmer=product.stemmer,
-            synonyms=Product.canonicalizations,
-        )
-
-    # Track the best match for each product
-    results = defaultdict(lambda: None)
-    scores = defaultdict(lambda: 0.0)
-    for candidate in find_product_candidates(products):
-        hits = execute_query(
-            index=description_index,
-            query=candidate.name,
-            stemmer=candidate.stemmer,
-            synonyms=Product.canonicalizations,
-        )
-        for hit in hits:
-            doc_id, score, terms = hit['doc_id'], hit['score'], hit['terms']
-            if score > scores[doc_id]:
-                results[doc_id] = candidate, terms
-                scores[doc_id] = score
-
-    # Build per-query result metadata
-    markup = defaultdict(lambda: None)
-    metadata = defaultdict(lambda: None)
-    for doc_id, (product, terms) in results.items():
-        description = descriptions[doc_id]
-        markup[doc_id] = highlight(
-            query=description,
-            terms=terms,
-            stemmer=product.stemmer,
-            synonyms=Product.canonicalizations,
-            case_sensitive=False,
-        )
-        metadata[doc_id] = product.get_metadata(description, app.graph, terms)
-
-    return jsonify({
-        'results': {
-            description: {
-                'product': metadata[doc_id],
-                'query': {
-                    'markup': markup[doc_id],
-                }
-            }
-            for doc_id, description in enumerate(descriptions)
-        }
-    })
+import web.ingredients
---
+++ web/ingredients.py
@@ -0,0 +1,87 @@
+from collections import defaultdict
+from flask import jsonify, request
+from hashedixsearch import (
+    add_to_search_index,
+    build_search_index,
+    execute_queries,
+    execute_query,
+    highlight,
+)
+
+from web.app import app
+from web.models.product import Product
+
+
+def find_product_candidates(products):
+    queries = [product.name for product in products]
+    results = execute_queries(
+        index=app.graph.index,
+        queries=queries,
+        stopwords=app.stopwords,
+        stemmer=Product.stemmer,
+        synonyms=Product.canonicalizations,
+        query_limit=-1
+    )
+    for description, hits in results:
+        for hit in hits:
+            product = app.graph.products_by_id[hit['doc_id']]
+            yield product
+
+
+@app.route('/ingredients/query', methods=['POST'])
+def query():
+    descriptions = request.form.getlist('descriptions[]')
+    products = [Product(name=description) for description in descriptions]
+
+    # Build a local search index over the product descriptions
+    description_index = build_search_index()
+    for doc_id, product in enumerate(products):
+        add_to_search_index(
+            index=description_index,
+            doc_id=doc_id,
+            doc=product.name,
+            stemmer=product.stemmer,
+            synonyms=Product.canonicalizations,
+        )
+
+    # Track the best match for each product
+    results = defaultdict(lambda: None)
+    scores = defaultdict(lambda: 0.0)
+    for candidate in find_product_candidates(products):
+        hits = execute_query(
+            index=description_index,
+            query=candidate.name,
+            stemmer=candidate.stemmer,
+            synonyms=Product.canonicalizations,
+        )
+        for hit in hits:
+            doc_id, score, terms = hit['doc_id'], hit['score'], hit['terms']
+            if score > scores[doc_id]:
+                results[doc_id] = candidate, terms
+                scores[doc_id] = score
+
+    # Build per-query result metadata
+    markup = defaultdict(lambda: None)
+    metadata = defaultdict(lambda: None)
+    for doc_id, (product, terms) in results.items():
+        description = descriptions[doc_id]
+        markup[doc_id] = highlight(
+            query=description,
+            terms=terms,
+            stemmer=product.stemmer,
+            synonyms=Product.canonicalizations,
+            case_sensitive=False,
+        )
+        metadata[doc_id] = product.get_metadata(description, app.graph, terms)
+
+    return jsonify({
+        'results': {
+            description: {
+                'product': metadata[doc_id],
+                'query': {
+                    'markup': markup[doc_id],
+                }
+            }
+            for doc_id, description in enumerate(descriptions)
+        }
+    })
