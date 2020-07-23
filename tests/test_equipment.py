0b3381194a6ffea30c67fbe8c8a9d8214b27c4ad
James Addison <james@reciperadar.com>
2020-07-23 18:29:32 +0100

    flake8

--- tests/test_equipment.py
+++ tests/test_equipment.py
@@ -1,20 +1,32 @@
 def test_description_parsing(client):
     description_equipment = {
         'Pre-heat the oven to 250 degrees F.': {
-            'markup': 'Pre-heat the <mark class="appliance">oven</mark> to 250 degrees F.',
+            'markup': (
+                'Pre-heat the <mark class="appliance">oven</mark> '
+                'to 250 degrees F.'
+            ),
             'appliances': [{'appliance': 'oven'}],
         },
         'leave the Slow cooker on a low heat': {
-            'markup': 'leave the <mark class="vessel">Slow cooker</mark> on a low heat',
+            'markup': (
+                'leave the <mark class="vessel">Slow cooker</mark> '
+                'on a low heat'
+            ),
             'appliances': [{'appliance': 'slow cooker'}],
         },
         'place casserole dish in oven': {
-            'markup': 'place <mark class="vessel">casserole dish</mark> in <mark class="appliance">oven</mark>',
+            'markup': (
+                'place <mark class="vessel">casserole dish</mark> '
+                'in <mark class="appliance">oven</mark>'
+            ),
             'appliances': [{'appliance': 'oven'}],
             'vessels': [{'vessel': 'casserole dish'}],
         },
         'empty skewer into the karahi': {
-            'markup': 'empty <mark class="utensil">skewer</mark> into the <mark class="vessel">karahi</mark>',
+            'markup': (
+                'empty <mark class="utensil">skewer</mark> into the '
+                '<mark class="vessel">karahi</mark>'
+            ),
             'utensils': [{'utensil': 'skewer'}],
             'vessels': [{'vessel': 'karahi'}],
         },
