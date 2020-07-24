983ebf961c380375a5156c4d3fa8d53a45f9d94c
James Addison <james@reciperadar.com>
2020-07-24 13:23:08 +0100

    Refactor: equipment endpoint: add a matrix of queries based on entity type and class

--- tests/test_equipment.py
+++ tests/test_equipment.py
@@ -1,42 +1,27 @@
 def test_description_parsing(client):
-    description_equipment = {
-        'Pre-heat the oven to 250 degrees F.': {
-            'markup': (
-                'Pre-heat the <mark class="appliance">oven</mark> '
-                'to 250 degrees F.'
-            ),
-            'appliances': [{'appliance': 'oven'}],
-        },
-        'leave the Slow cooker on a low heat': {
-            'markup': (
-                'leave the <mark class="appliance">Slow cooker</mark> '
-                'on a low heat'
-            ),
-            'appliances': [{'appliance': 'slow cooker'}],
-        },
-        'place casserole dish in oven': {
-            'markup': (
-                'place <mark class="vessel">casserole dish</mark> '
-                'in <mark class="appliance">oven</mark>'
-            ),
-            'appliances': [{'appliance': 'oven'}],
-            'vessels': [{'vessel': 'casserole dish'}],
-        },
-        'empty skewer into the karahi': {
-            'markup': (
-                'empty <mark class="utensil">skewer</mark> into the '
-                '<mark class="vessel">karahi</mark>'
-            ),
-            'utensils': [{'utensil': 'skewer'}],
-            'vessels': [{'vessel': 'karahi'}],
-        },
+    description_markup = {
+        'Pre-heat the oven to 250 degrees F.': (
+            'Pre-heat the <mark class="equipment appliance">oven</mark> '
+            'to 250 degrees F.'
+        ),
+        'leave the Slow cooker on a low heat': (
+            'leave the <mark class="equipment appliance">Slow cooker </mark> '
+            'on a low heat'
+        ),
+        'place casserole dish in oven': (
+            'place <mark class="equipment vessel">casserole dish</mark> '
+            'in <mark class="equipment appliance">oven</mark>'
+        ),
+        'empty skewer into the karahi': (
+            'empty <mark class="equipment utensil">skewer</mark> into the '
+            '<mark class="equipment vessel">karahi</mark>'
+        ),
     }
 
     response = client.post(
         '/equipment/query',
-        data={'descriptions[]': list(description_equipment.keys())}
+        data={'descriptions[]': list(description_markup.keys())}
     )
     for result in response.json:
-        assert result['description'] in description_equipment
-        for key, value in description_equipment[result['description']].items():
-            assert result[key] == value
+        assert result['description'] in description_markup
+        assert result['markup'] == description_markup[result['description']]
