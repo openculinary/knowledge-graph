214073e7598e17fcbc4f43c095795ad2c141f216
James Addison <james@reciperadar.com>
2020-11-13 16:40:08 +0000

    Add endpoint to retrieve individual product metadata

---
+++ web/products.py
@@ -0,0 +1,20 @@
+from flask import abort, jsonify
+
+from web import app
+
+
+@app.route('/products/<product_id>')
+def product(product_id):
+    product = app.graph.products_by_id.get(product_id)
+    if not product:
+        return abort(404)
+
+    metadata = product.get_metadata(product.name, app.graph)
+
+    return jsonify({
+        'results': {
+            product.name: {
+                'product': metadata,
+            }
+        }
+    })
