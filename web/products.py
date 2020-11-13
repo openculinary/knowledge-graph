e2a3b5f9f6dbfd7a0a6190c2650a9008518170ab
James Addison <james@reciperadar.com>
2020-11-13 18:27:33 +0000

    Simplify response format

--- web/products.py
+++ web/products.py
@@ -8,13 +8,4 @@ def product(product_id):
     product = app.graph.products_by_id.get(product_id)
     if not product:
         return abort(404)
-
-    metadata = product.get_metadata(product.name, app.graph)
-
-    return jsonify({
-        'results': {
-            product.name: {
-                'product': metadata,
-            }
-        }
-    })
+    return jsonify(product.get_metadata(product.name, app.graph))
